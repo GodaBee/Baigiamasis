0ef0bd26564cdbe88f7be1a48f195f903682bb49
GodaBee <goda.butrimaviciute@gmail.com>
2023-09-27 19:02:45 +0300

    Main

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
-   // Initial function to show buttons and hide content
+
    function showButtons() {
-    // Show the set of buttons
     var buttonsDiv = document.querySelector(".buttons");
     if (buttonsDiv) {
         buttonsDiv.style.display = "block";
     }
-
-    // Hide the "Show Buttons" button (if needed)
     var showButtonsButton = document.getElementById("showButtons");
     if (showButtonsButton) {
         showButtonsButton.style.display = "none";
     }
 
-    // Hide all content initially
     var contentDivs = document.querySelectorAll(".buttonContent");
     for (var i = 0; i < contentDivs.length; i++) {
         contentDivs[i].style.display = "none";
     }
 }
-
-// Call the showButtons function to initially show only the buttons
 showButtons();
 
-// Your existing pakeisk function remains unchanged
 function pakeisk(id) {
-    // Hide all divs with class "buttonContent"
     var divs = document.querySelectorAll(".buttonContent");
     for (var i = 0; i < divs.length; i++) {
         divs[i].style.display = "none";
     }
-
-    // Show the selected div by id
     var selectedDiv = document.getElementById(id);
     if (selectedDiv) {
         selectedDiv.style.display = "block";
     }
 }
+
+// var burger = document.getElementById("burger");
+// var links2 = document.getElementById("links2");
+
+// Add an event listener to the burger icon
+// burger.addEventListener("click", function() {
+//   links2.classList.toggle("translate");
+//   this.classList.toggle("rotate");
+// });
+
+// Add an event listener to each anchor tag inside links2
+// var anchorTags = links2.querySelectorAll("a");
+// anchorTags.forEach(function(anchor) {
+//   anchor.addEventListener("click", function() {
+//     // Remove the translate class when an anchor is clicked
+//     links2.classList.remove("translate");
+//     burger.classList.remove("rotate");
+//   });
+// });
+
+// Add a window resize event listener
+// window.addEventListener("resize", function() {
+//   if (this.innerWidth > 768) {
+//     links2.classList.remove("translate");
+//     burger.classList.remove("rotate");
+//   }
+// });
+
